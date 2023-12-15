db58f3094ab3557e6cac6fc0137b27cf519dc249
2cec45 <hannes.matthes@gmx.de>
2023-12-15 22:41:31 +0100

    "accelerometer Added"

--- function.js
+++ function.js
@@ -1,14 +1,12 @@
-if (window.DeviceOrientationEvent) {
-
-} else {
-    console.log('Gyroskop wird nicht unterstützt');
-}
-
 let gyroscope = new Gyroscope({ frequency: 30 });
+let accelerometer = new Accelerometer({frequency: 30});
 
 gyroscope.addEventListener("reading", (e) => {
-    document.getElementById("accelerationSpan").innerText = `Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
+    document.getElementById("gyroscopeSpan").innerText = "Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}";
 });
+accelerometer.addEventListener("reading", (e) => {
+    document.getElementById("accelerometerSpan").innerText = "Acceleration: ${accelerometer.x, ${$accelerometer.y}, ${$accelerometer.z}}";
+})
 function setStartPoint(){
     gyroscope.start();
 }
