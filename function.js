f4d0cf00798b3eb38a707d650df5ac8433e8898f
2cec45 <hannes.matthes@gmx.de>
2023-12-15 22:23:07 +0100

    "ttest"

--- function.js
+++ function.js
@@ -9,5 +9,6 @@ let gyroscope = new Gyroscope({ frequency: 30 });
 gyroscope.addEventListener("reading", (e) => {
     document.getElementById("accelerationSpan").innerText = `Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
 });
-
-gyroscope.start();
+function setStartPoint(){
+    gyroscope.start();
+}
