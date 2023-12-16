e07ecdc3711919a497d0a755f038579fc76e80d0
2cec45 <hannes.matthes@gmx.de>
2023-12-16 16:12:20 +0100

    css hinzugefügt und aufgeräumt

--- function.js
+++ function.js
@@ -1,5 +1,9 @@
 let gyroscope = new Gyroscope({ frequency: 30 });
 let accelerometer = new Accelerometer({ frequency: 30 });
+let startPoint = new Float64Array(3);
+startPoint[0] = 0;
+startPoint[1] = 0;
+startPoint[2] = 0;
 
 gyroscope.addEventListener("reading", (e) => {
     document.getElementById("gyroscopeSpan").innerText = `Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
@@ -8,8 +12,9 @@ gyroscope.addEventListener("reading", (e) => {
 accelerometer.addEventListener("reading", (e) => {
     document.getElementById("accelerometerSpan").innerText = `Acceleration: ${accelerometer.x}, ${accelerometer.y}, ${accelerometer.z}`;
 });
+gyroscope.start();
+accelerometer.start();
 
 function setStartPoint() {
-    gyroscope.start();
-    accelerometer.start();
+
 }
