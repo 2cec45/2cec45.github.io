b2ff7b79b3cecbba1e09ba9a39af71b7730bd268
2cec45 <hannes.matthes@gmx.de>
2023-12-16 15:32:13 +0100

    gpt version

--- function.js
+++ function.js
@@ -1,13 +1,15 @@
 let gyroscope = new Gyroscope({ frequency: 30 });
-let accelerometer = new Accelerometer({frequency: 30});
+let accelerometer = new Accelerometer({ frequency: 30 });
 
 gyroscope.addEventListener("reading", (e) => {
-    document.getElementById("gyroscopeSpan").innerText = "Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}";
+    document.getElementById("gyroscopeSpan").innerText = `Angular velocity: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
 });
+
 accelerometer.addEventListener("reading", (e) => {
-    document.getElementById("accelerometerSpan").innerText = "Acceleration: ${accelerometer.x, ${$accelerometer.y}, ${$accelerometer.z}}";
+    document.getElementById("accelerometerSpan").innerText = `Acceleration: ${accelerometer.x}, ${accelerometer.y}, ${accelerometer.z}`;
 });
-function setStartPoint(){
+
+function setStartPoint() {
     gyroscope.start();
     accelerometer.start();
 }
