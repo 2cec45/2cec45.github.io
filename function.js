ed757568f74b9f15510a17fec86ccf680b003dd0
2cec45 <hannes.matthes@gmx.de>
2023-12-17 14:27:08 +0100

    test width/height

--- function.js
+++ function.js
@@ -4,14 +4,8 @@ let startPoint = new Float64Array(3);
 startPoint[0] = 0;
 startPoint[1] = 0;
 startPoint[2] = 0;
-setCanvasSize();
 
 function setCanvasSize() {
-    var drawing_canvas = document.getElementById("drawing_canvas");
-    drawing_canvas.style.backroundColor = "blue";
-    console.log("changed");
-    drawing_canvas.style.width = window.screen.availWidth;
-    drawing_canvas.style.height = window.screen.availHeight;
 }
 
 gyroscope.addEventListener("reading", (e) => {
