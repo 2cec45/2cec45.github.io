d8ab218f4803ce3d3660fa3d14c82ef022a26c6d
2cec45 <hannes.matthes@gmx.de>
2023-12-21 17:28:31 +0100

    shooting added

--- function.js
+++ function.js
@@ -10,14 +10,30 @@ class Stone {
     }
 }
 
-class Vector{
+class Vector {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
+class Bullet {
+    constructor(position, velocity, direction) {
+        this.position = position;
+        this.velocity = velocity;
+        this.direction = direction;
+    }
+}
+
+// globals
+var currentFrame = 0;
+// GameParameters
 const stoneAmount = 6;
+const framesPerBullet = 5;
+const stones = new Array(5);
+const middle = new Vector(500, 500);
+// PlayerParameters
+var currentRotation = 0;
 const playerOrientation = new Int32Array(stoneAmount);
 const tipOrigin = new Vector(0, 20);
 const lCOrigin = new Vector(10, -20);
@@ -25,72 +41,94 @@ const rCOrigin = new Vector(-10, -20);
 var tip = new Vector(0, 20);
 var lC = new Vector(10, -20);
 var rC = new Vector(-10, -20);
-const stones = new Array(5);
-const middle = new Vector(500, 500);
-var currentRotation = 0;
 // StoneParameters:
 const xRange = new Vector(-500, 500);
 const yRange = new Vector(-500, 500);
-const sizeRange =  new Vector(10, 30);
+const sizeRange = new Vector(10, 30);
 const velocityRange = new Vector(2, 5);
+// BulletParameters
+const bulletVelocity = 4;
+const bulletSize = new Vector(7, 2);
+const bullets = new Array(0);
 
 
+function add(vec1, vec2) {
+    var result = new Vector(vec1.x + vec2.x, vec1.y + vec2.y);
+    return result;
+}
+
+function scalarMult(vec, scalar) {
+    var res = new Vector(0, 0);
+    res.x = vec.x * scalar;
+    res.y = vec.y * scalar;
+    return res;
+}
 
 function rotate(vec, deg) {
-    var result = new Vector(0,0);
+    var result = new Vector(0, 0);
     result.x = vec.x * Math.cos(deg) + vec.y * (- Math.sin(deg));
     result.y = vec.x * Math.sin(deg) + vec.y * Math.cos(deg);
     return result;
+}
+
+function playerShoot(tip, lC, rC) {
+    bullets.push(new Bullet(tip, bulletVelocity, normalize(tip)));
+}
+
+function drawBullet(context, bullet) {
+    context.lineWidth = bulletSize.y;
+    context.beginPath();
+    context.moveTo(bullet.position.x, bullet.position.y);
+    var newPosition = add(bullet.position, scalarMult(bullet.direction, bulletSize.x));
+    context.lineTo(newPosition.x, newPosition.y);
+    context.stroke();
+    context.closePath();
+    context.lineWidth = 1;
+}
+
+function drawBullets(context) {
+    for (let i = 0; i < bullets.length; i++) {
+        drawBullet(context, bullets[i]);
+    }
 }
 
 function randomStonePosition() {
     var number = Math.trunc(Math.random() * 4);
     switch (number) {
         case 1:
-            return new Vector(500, Math.random() * (yRange.y-yRange.x) + yRange.x);
+            return new Vector(500, Math.random() * (yRange.y - yRange.x) + yRange.x);
         case 2:
-            return new Vector(-500, Math.random() * (yRange.y-yRange.x) + yRange.x);
+            return new Vector(-500, Math.random() * (yRange.y - yRange.x) + yRange.x);
         case 3:
-            return new Vector(Math.random() * (yRange.y-yRange.x) + yRange.x , 500);
+            return new Vector(Math.random() * (yRange.y - yRange.x) + yRange.x, 500);
         default:
-            return new Vector(Math.random() * (yRange.y-yRange.x) + yRange.x , -500);
+            return new Vector(Math.random() * (yRange.y - yRange.x) + yRange.x, -500);
     }
 }
 
-function add(vec1, vec2) {
-    vec1.x = vec1.x + vec2.x;
-    vec1.y = vec1.y + vec2.y;
-}
+function wallCollisionDetection() {
 
-function scalarMult(vec, scalar){
-    var res = new Vector(0,0);
-    res.x = vec.x * scalar;
-    res.y = vec.y * scalar;
-    return res;
-}
-
-function wallCollisionDetection(){
-    
     for (let i = 0; i < stones.length; i++) {
-        if((stones[i].position.x > 550) || (stones[i].position.x < -550) ||
-         (stones[i].position.y < -550) || (stones[i].position.y > 550)){
+        if ((stones[i].position.x > 550) || (stones[i].position.x < -550) ||
+            (stones[i].position.y < -550) || (stones[i].position.y > 550)) {
             stones[i] = createRandomStone(xRange, yRange, sizeRange, velocityRange);
         }
     }
 }
 
 function normalize(vec) {
-    var length = Math.sqrt((Math.pow(vec.x,2) + Math.pow(vec.y,2)));
-    return new Vector(vec.x/length, vec.y/length);
+    var length = Math.sqrt((Math.pow(vec.x, 2) + Math.pow(vec.y, 2)));
+    return new Vector(vec.x / length, vec.y / length);
 }
-function createRandomStone(xRange, yRange, sizeRange, velocityRange){
+function createRandomStone(xRange, yRange, sizeRange, velocityRange) {
     position = randomStonePosition();
-    var size = Math.random()*(sizeRange.y - sizeRange.x) + sizeRange.x;
+    var size = Math.random() * (sizeRange.y - sizeRange.x) + sizeRange.x;
     var directionNormalized = normalize(position);
     var direction = new Vector(-directionNormalized.x, -directionNormalized.y);
-    var velocity = Math.random()*(velocityRange.y - velocityRange.x) + velocityRange.x;
+    var velocity = Math.random() * (velocityRange.y - velocityRange.x) + velocityRange.x;
     return new Stone(position, size, direction, velocity);
 }
+
 function fillStonesArray(xRange, yRange, sizeRange, velocityRange) {
     for (let i = 0; i < stones.length; i++) {
         stones[i] = createRandomStone(xRange, yRange, sizeRange, velocityRange);
@@ -99,11 +137,11 @@ function fillStonesArray(xRange, yRange, sizeRange, velocityRange) {
 
 var context;
 var st2;
-function init(){
+function init() {
     const canvas = document.getElementById("drawing_canvas");
     context = canvas.getContext("2d");
     context.translate(middle.x, middle.y);
-    fillStonesArray(xRange,yRange,sizeRange, velocityRange);
+    fillStonesArray(xRange, yRange, sizeRange, velocityRange);
     window.requestAnimationFrame(draw);
 }
 
@@ -124,16 +162,21 @@ function drawPlayer(context) {
     context.closePath()
 }
 
-function updatePositions(){
+function updatePositions() {
     //update Player
     currentRotation += 0.1;
-    tip = rotate(tipOrigin,currentRotation / Math.PI*2);
-    lC = rotate(lCOrigin,currentRotation / Math.PI*2);
-    rC = rotate(rCOrigin,currentRotation / Math.PI*2);
+    tip = rotate(tipOrigin, currentRotation / Math.PI * 2);
+    lC = rotate(lCOrigin, currentRotation / Math.PI * 2);
+    rC = rotate(rCOrigin, currentRotation / Math.PI * 2);
     //update Stones
     for (let i = 0; i < stones.length; i++) {
         var mult = scalarMult(stones[i].direction, stones[i].velocity);
-        add(stones[i].position, mult);
+        stones[i].position = add(stones[i].position, mult);
+    }
+    //update Bullets
+    for (let i = 0; i < bullets.length; i++) {
+        var mult = scalarMult(bullets[i].direction, bulletVelocity);
+        bullets[i].position = add(bullets[i].position, mult);
     }
     wallCollisionDetection();
 }
@@ -145,21 +188,27 @@ function drawStone(context, stone) {
     context.closePath();
 }
 
-function drawStones(context){
+function drawStones(context) {
     for (let i = 0; i < stones.length; i++) {
         const element = stones[i];
         drawStone(context, element);
     }
 }
 
-function draw(){
+function draw() {
     var height = window.innerHeight;
     var width = window.innerWidth;
-    context.clearRect(-500,-500, 1000, 1000)
+    currentFrame++;
+    context.clearRect(-500, -500, 1000, 1000)
     updatePositions();
+    if((currentFrame % framesPerBullet) == 0) {
+        playerShoot(tip, lC, rC);
+    }
     drawPlayer(context);
+    drawBullets(context);
     drawStones(context, stones);
-    window.requestAnimationFrame(draw)
+    console.log(bullets);
+    window.requestAnimationFrame(draw);
 }
 
 /*gyroscope.addEventListener("reading", (e) => {
